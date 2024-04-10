f817d5af380eee732539d8be62d3a58c0a40b462
sayaliambure <sayaliambure15@gmail.com>
2024-04-10 22:45:09 +0530

    changes

--- base/urls.py
+++ base/urls.py
@@ -20,3 +20,6 @@ urlpatterns = [
 
 ]
 # str-string, pk-primary key
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
