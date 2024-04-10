c835f0bbbf39aea3b514922c9eed065a7174b9d8
sayaliambure <sayaliambure15@gmail.com>
2024-04-10 22:56:08 +0530

    changes

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@
 
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
   path('login/', views.loginPage, name="login"),
