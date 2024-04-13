1c75c992e6ac3e1671ec7098c9210e6bf5a32cef
sayaliambure <sayaliambure15@gmail.com>
2024-04-13 13:18:04 +0530

    changes

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-qy4^=bh(h_&wqqced+c_v801xme-69o%ke-#kefg5^v6w_22x)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', ".now.sh"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -137,6 +137,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True
 # would allow all other websites to access our websites
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# import os
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
